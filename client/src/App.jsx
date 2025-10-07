d19685ee6b6db1b0031bf9a4c8d7f430b91c95e9
Nguyen Huu Thang <nguyenhthang1234@gmail.com>
2025-10-07 20:36:31 +0700

    Add files via upload

---
+++ client/src/App.jsx
@@ -0,0 +1,21 @@
+import NavBar from "./components/NavBar";
+import SignUpForm from "./components/SignUpForm";
+import SignInForm from "./components/SignInForm";
+import HomePage from "./components/HomePage";
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+function App() {
+    return (
+        <Router>
+            <NavBar />
+            <Routes>
+                <Route path="/signup" element={<SignUpForm />} />
+                <Route path="/signin" element={<SignInForm />} />
+                <Route path="/homepage" element={<HomePage />} />
+            </Routes>
+        </Router>
+    );
+}
+
+export default App
