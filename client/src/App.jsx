582667d54905b50b1d88ce233eea4c84cd5639d7
Nguyen Huu Thang <nguyenhthang1234@gmail.com>
2025-10-08 17:55:41 +0700

    Add files via upload

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,11 +2,13 @@ import NavBar from "./components/NavBar";
 import SignUpForm from "./components/SignUpForm";
 import SignInForm from "./components/SignInForm";
 import HomePage from "./components/HomePage";
+import AuthProvider from "./contexts/AuthProvider"
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
     return (
+        <AuthProvider>
         <Router>
             <NavBar />
             <Routes>
@@ -16,6 +18,7 @@ function App() {
                 <Route path="/homepage" element={<HomePage />} />
             </Routes>
         </Router>
+        </AuthProvider>
     );
 }
 
