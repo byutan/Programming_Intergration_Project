1623017ed8442aeacccec9d1dffaf157e8ce0508
Nguyen Huu Thang <nguyenhthang1234@gmail.com>
2025-10-08 17:57:54 +0700

    Add files via upload

--- server/src/app.js
+++ server/src/app.js
@@ -3,21 +3,24 @@ import serveFavicon from 'serve-favicon';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import cors from 'cors'
-import signUpRouter from './routes/signUp.js'
+
+import signUpRouter from './routes/signup.js'
+import signInRouter from './routes/signin.js'
 
 const __fileName = fileURLToPath(import.meta.url);
 const __dirName = path.dirname(__fileName);
 const app = express();
 
 app.use(serveFavicon(path.join(__dirName, '../public', 'favicon.ico')));
-app.use(express.json())
+app.use(express.json());
 app.use(cors());
-
-app.use('/api', signUpRouter)
 
 app.get('/', function (req, res) {
     res.send('Hello World!');
 });
+
+app.use('/api', signUpRouter);
+app.use('/api', signInRouter);
 
 
 export default app;
